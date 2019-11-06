e7ae6cd1d023f392380f08baa5f02627e2c824e0
Nicolai Larsen <nicolailarsen2100@gmail.com>
2019-11-06 18:22:39 +0100

    listview kører

--- app/src/main/java/com/s185020/Galgeleg_2/fragments/HighScoreFragment.java
+++ app/src/main/java/com/s185020/Galgeleg_2/fragments/HighScoreFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,22 +24,19 @@ import com.s185020.Galgeleg_2.R;
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 public class HighScoreFragment extends Fragment implements View.OnClickListener {
 
     private Button button_play, button_settings, button_save, button_load;
     private TextView textView;
-    private TextView textView_highScore;
     private ListView listView;
     private String word;
     private int choice;
-    private int score;
+    private double score;
     private int wrongGuessCount;
     private boolean won;
     private static final String FILE_NAME = "example.txt";
@@ -76,20 +74,17 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
             word = getArguments().getString("word");
         }
         textView = highScore_view.findViewById(R.id.textView_highscore);
-        textView_highScore = highScore_view.findViewById(R.id.textView_highscore);
         button_play = highScore_view.findViewById(R.id.button_play_from_main);
         button_play.setText("Play");
         button_settings = highScore_view.findViewById(R.id.button_settings);
         button_settings.setText("Settings");
-        button_save = highScore_view.findViewById(R.id.button_save);
-        button_load = highScore_view.findViewById(R.id.button_load);
+
         listView = highScore_view.findViewById(R.id.list_view);
 
 //        listView.setOnItemClickListener((AdapterView.OnItemClickListener) settings_view);
         button_play.setOnClickListener(this);
         button_settings.setOnClickListener(this);
-        button_save.setOnClickListener(this);
-        button_load.setOnClickListener(this);
+
 
         // listView.setAdapter(new ArrayAdapter(this, a));
 
@@ -102,8 +97,8 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
         }
 
         //todo færdiggør
-        score = (1000 / 7) * (7 - wrongGuessCount);
-        Date date = new Date(); // this object contains the current date value
+        score = Math.ceil((1000.0 / 7.0) * (7.0 - wrongGuessCount));
+        Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
 
         currentScoreString = ("" + (10000 + score)).substring(1) + " points - " + formatter.format(date);
@@ -116,17 +111,13 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
         loadData();
         addCurrentDataToList();
         saveData();
-
         snap();
-
+        listView.setAdapter(new ArrayAdapter(getContext(), R.layout.listview_layout, R.id.list_element, highscoreList));
         return highScore_view;
     }
 
     private void addCurrentDataToList() {
         highscoreList.add(currentScoreString);
-
-        //List<String> list = Arrays.asList("def123", "abc999", "zzz000", "abc123", "zzz111");
-        //List<String> list = highscoreList.subList(0,highscoreList.size());
 
         Comparator<String> cmp = new Comparator<String>() {
             public int compare(String o1, String o2) {
@@ -135,30 +126,9 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
                 return diff;
             }
         };
-        Collections.sort(highscoreList, cmp);
+        Collections.sort(highscoreList, Collections.reverseOrder());
 
-  /*      HashMap<String, String> stringMap = new HashMap<>();
-        List <String> highscoreList2 = new ArrayList<>();
-
-        for (String string : highscoreList) {
-            stringMap.put(string.substring(0, 5), string.substring(5));
-            highscoreList2.add(string.substring(0, 5));
-        }
-        Collections.sort(highscoreList2);
-
-        highscoreList.clear();
-        for (String string : highscoreList2) {
-            highscoreList.add(string + stringMap.get(string));
-        }
-
-
-        try {
-            highscoreList = highscoreList.subList(0, 11);
-        } catch (Exception e) {
-
-        }
-        */
-
+        //Collections.sort(highscoreList, cmp);
     }
 
     private void saveData() {
@@ -170,7 +140,6 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
     }
 
     private void loadData() {
-        //       sharedPreferences = getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
         Gson gson = new Gson();
         String json = sharedPreferences.getString(SAVED_LIST, null);
         Type type = new TypeToken<ArrayList<String>>() {
@@ -183,10 +152,10 @@ public class HighScoreFragment extends Fragment implements View.OnClickListener 
     }
 
     void snap() {
-if (highscoreList != null)
-        for (String string : highscoreList) {
-            System.out.println(string);
-        }
+        if (highscoreList != null)
+            for (String string : highscoreList) {
+                System.out.println(string);
+            }
     }
 
     public void onClick(View v) {
@@ -201,22 +170,6 @@ if (highscoreList != null)
 
         }
     }
-
-    /*public void printToTextview() {
-
-        textView_highScore.setText(load());
-
-    }*/
-
-  /*
-
-    private void loadData() {
-        SharedPreferences sharedPreferences = MainUI.getContextOfApp().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
-        currentRadioChannel = sharedPreferences.getFloat(SAVED_LIST, 0.0f);
-        currentRadioFrequency = sharedPreferences.getString(CURRENT_RADIOFREQUENCY, "FM");
-    }
-
-*/
 
     //todo kan det gøres smartere?
     //ny knap -> finis går tilbage til forrige aktivitet
