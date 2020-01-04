52af0f1020af2b6985a682279a18f37b42654650
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-01-04 10:21:48 +0100

    Implementeret help fragment

---
+++ app/src/main/java/com/s185020/Galgeleg_2/fragments/HelpFragment.java
@@ -0,0 +1,34 @@
+package com.s185020.Galgeleg_2.fragments;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+import com.s185020.Galgeleg_2.R;
+
+public class HelpFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View help_view = inflater.inflate(R.layout.fragment_help, container, false);
+
+        return help_view;
+    }
+
+
+    private void openSettings() {
+        getFragmentManager().beginTransaction().replace(R.id.fragmentLayout, SettingsFragment.getInstance())
+                .addToBackStack(null)
+                .commit();
+
+
+    }
+}
